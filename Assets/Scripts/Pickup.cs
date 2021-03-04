47d190e9f548f4f3852a65764b6eaf7141a13b56
Ross Bown <ross88399@outlook.com>
2021-03-04 23:44:43 +0000

    fixing shit

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public static bool canPickUp = true;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (canPickUp)
+            {
+                GameManager.instance.addItem(GetComponent<Item>().itemName);
+                GetComponent<Item>().gameObject.SetActive(false);
+            }            
+        }
+    }
+}
